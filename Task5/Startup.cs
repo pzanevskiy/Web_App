6d5ecf9828d57008556727e9d9ee01aeaacd2de5
p.zanevskiy <pavel.zanevsky15@gmail.com>
2021-02-26 01:09:14 +0300

    Добавьте файлы проекта.

---
+++ Task5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Task5.Startup))]
+namespace Task5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
